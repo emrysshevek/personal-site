cafa6f6f88c31a998d522870cb6cc770aed62ad4
Emrys Shevek <emrysshevek@gmail.com>
2026-06-10 19:07:40 -0700

    as good as it's gonna get for now

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
-  return render(request, "home/index.html")
+  return render(request, "home/index.html", {"start": timezone.now})
